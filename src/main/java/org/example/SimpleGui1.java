40298257327d3b00f07ed22fdfccd1a829836382
Francesco-Napolitano <francesco.46napolitano@gmail.com>
2025-03-04 22:41:28 +0100

    first GUI con backend

---
+++ src/main/java/org/example/SimpleGui1.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import javax.swing.*;
+
+public class SimpleGui1 {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        JButton button = new JButton("Click me");
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //serve solo a spegnere il programma quando si vuole chiudere la finestra
+
+        frame.getContentPane().add(button);
+
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+    }
+}
